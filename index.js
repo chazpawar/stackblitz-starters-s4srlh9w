c5b143b98694c66c18a39de311f84082dc739a53
chazpawar <chaitanya09pawar@gmail.com>
2025-02-25 10:39:11 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const { resolve } = require('path');
+const mongoose = require("mongoose")
+const dotenv = require("dotenv")
+
+const app = express();
+const port = 3010;
+
+app.use(express.static('static'));
+
+const connectDB = async()=>{
+  try{
+    await mongoose.connect(process.env.MONGO_URI)
+    console.log(`Connected to database`)
+
+  }catch(error){
+    console.error(`Error connecting to database: <Error details>`)
+    process.exit(1)
+
+  }
+}
+app.get('/', (req, res) => {
+  res.sendFile(resolve(__dirname, 'pages/index.html'));
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
